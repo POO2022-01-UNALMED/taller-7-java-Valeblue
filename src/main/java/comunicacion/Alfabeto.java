adf6a56b4a0aef03b4492890323b45df4d86b471
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-05-18 20:57:01 -0500

    :100:
    Inicios de la Lectura

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,57 @@
+package comunicacion;
+
+
+import java.util.Arraylist;
+
+public class Alfabeto extends Pictograma  {
+
+	private Strings[]letras;
+	private Strings interpretacion;
+	
+	
+	
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public int cantidadLetras(){
+        return letras.length;
+    }
+
+    public String interpretacion(){
+        return this.interpretacion;
+    }
+
+    public String toString(){
+        String todo_alfabeto = "";
+        for ( int i = 0; i < letras.length; i++){
+            if ( i != letras.length - 1 ){
+                todo_alfabeto += letras[i] + ", ";
+            } else {
+                todo_alfabeto += letras[i];
+            }
+        }
+        return todo_alfabeto;
+    }
+
+}
+}
