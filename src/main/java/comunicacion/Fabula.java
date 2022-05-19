1c4c5c3af10c84362dd01f76fa7faf69d9959e1c
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-05-18 21:20:48 -0500

    Libros

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,60 +1,56 @@
 package comunicacion;
 
+public class Fabula extends Escrito{
+    private static final int factor = 1;
+    private String ensenanza;
+    private String interpretacion;
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.ensenanza = ensenanza;
+        this.interpretacion = interpretacion;
+    }
 
+    public String getEnsenanza() {
+        return ensenanza;
+    }
 
+    public void setEnsenanza(String ensenanza) {
+        this.ensenanza = ensenanza;
+    }
 
+    public String getInterpretacion() {
+        return interpretacion;
+    }
 
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
 
+    @Override
+    public int palabrasTotales(int palabrasPagina) {
+        return this.getPaginas() * palabrasPagina * factor;
+    }
 
+    @Override
+    public String interpretacion() {
+        return this.interpretacion;
+    }
 
+    @Override
+    public String toString() {
+        return this.getOrigen() +
+                "\n" + this.getTitulo() +
+                "\n" + this.getAutor() +
+                "\n" + this.getPaginas() +
+                "\n" + this.ensenanza;
+    }
+    
+    
+    
+    
+    
+    
+}
 
-public class Fabula extends Escrito {
-	
-	
-	 private static final int factor = 1;
-	    private String ensenanza;
-	    private String interpretacion;
-	    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
-	        super(origen, titulo, autor, paginas);
-	        this.ensenanza = ensenanza;
-	        this.interpretacion = interpretacion;
-	    }
-
-	    public String getEnsenanza() {
-	        return ensenanza;
-	    }
-
-	    public void setEnsenanza(String ensenanza) {
-	        this.ensenanza = ensenanza;
-	    }
-
-	    public String getInterpretacion() {
-	        return interpretacion;
-	    }
-
-	    public void setInterpretacion(String interpretacion) {
-	        this.interpretacion = interpretacion;
-	    }
-
-	    @Override
-	    public int palabrasTotales(int palabrasPagina) {
-	        return this.getPaginas() * palabrasPagina * factor;
-	    }
-
-	    @Override
-	    public String interpretacion() {
-	        return this.interpretacion;
-	    }
-
-	    @Override
-	    public String toString() {
-	        return this.getOrigen() +
-	                "\n" + this.getTitulo() +
-	                "\n" + this.getAutor() +
-	                "\n" + this.getPaginas() +
-	                "\n" + this.ensenanza;
-	    }
-	}
-	
 
 
