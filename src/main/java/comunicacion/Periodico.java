7045b78e45c1f127cb3582d638d84655fcfd88ae
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-05-18 21:24:56 -0500

    Update Periodico.java

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -1,62 +1,58 @@
 package comunicacion;
 
-public class Periodico {
-	
-	public class Periodico extends Escrito{
-	    private static final int factor = 10;
-	    private String fecha;
-	    private String primicia;
-	    private String interpretacion;
-	    public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
-	        super(origen, titulo, autor, paginas);
-	        this.fecha = fecha;
-	        this.primicia = primicia;
-	        this.interpretacion = interpretacion;
-	    }
+public class Periodico extends Escrito{
+    private static final int factor = 10;
+    private String fecha;
+    private String primicia;
+    private String interpretacion;
+    public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.fecha = fecha;
+        this.primicia = primicia;
+        this.interpretacion = interpretacion;
+    }
 
-	    public String getFecha() {
-	        return fecha;
-	    }
+    public String getFecha() {
+        return fecha;
+    }
 
-	    public void setFecha(String fecha) {
-	        this.fecha = fecha;
-	    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 
-	    public String getPrimicia() {
-	        return primicia;
-	    }
+    public String getPrimicia() {
+        return primicia;
+    }
 
-	    public void setPrimicia(String primicia) {
-	        this.primicia = primicia;
-	    }
+    public void setPrimicia(String primicia) {
+        this.primicia = primicia;
+    }
 
-	    public String getInterpretacion() {
-	        return interpretacion;
-	    }
+    public String getInterpretacion() {
+        return interpretacion;
+    }
 
-	    public void setInterpretacion(String interpretacion) {
-	        this.interpretacion = interpretacion;
-	    }
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
 
-	    @Override
-	    public int palabrasTotales(int palabrasPagina) {
-	        return this.getPaginas() * palabrasPagina * factor;
-	    }
+    @Override
+    public int palabrasTotales(int palabrasPagina) {
+        return this.getPaginas() * palabrasPagina * factor;
+    }
 
-	    @Override
-	    public String interpretacion() {
-	        return this.interpretacion;
-	    }
+    @Override
+    public String interpretacion() {
+        return this.interpretacion;
+    }
 
-	    @Override
-	    public String toString() {
-	        return this.getOrigen() +
-	                "\n" + this.getTitulo() +
-	                "\n" + this.getAutor() +
-	                "\n" + this.getPaginas() +
-	                "\n" + this.fecha +
-	                "\n" + this.primicia;
-	    }
-	}
-
+    @Override
+    public String toString() {
+        return this.getOrigen() +
+                "\n" + this.getTitulo() +
+                "\n" + this.getAutor() +
+                "\n" + this.getPaginas() +
+                "\n" + this.fecha +
+                "\n" + this.primicia;
+    }
 }
